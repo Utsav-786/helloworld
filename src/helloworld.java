654e74dbb7db355892b97433d8655a798853f884
Utsav-786 <utsav.singh_cs16@gla.ac.in>
2019-02-25 23:24:38 +0530

    initial commit

---
+++ src/helloworld.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Aman
+ */
+public class helloworld {
+    public static void maain(String[] args){
+        System.out.println("helloworld");
+        System.out.println("hello");
+    }
+}
